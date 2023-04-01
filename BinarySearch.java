b713ac769b009ed08b6675f9cadeb3f4786367d6
anuroopkanna <anuroopindia16@gmail.com>
2023-04-01 20:34:59 +0530

    added:uc1

--- BinarySearch.java
+++ BinarySearch.java
@@ -3,5 +3,25 @@ package com.BridgeLabzDay15;
 public class BinarySearch<T extends Comparable<T>> {
     INode<T> root;
     INode<T> currentPointer;
+//uc1
+public void add(T data){
+    INode<T> newNode = new INode<>(data);
+    if(root == null) {
+        root = newNode;
+        return;
+    }
+    INode<T> currentPointer= root;
+    while(currentPointer.NextRN != null && currentPointer.NextLN != null) {
+        if (data.compareTo(currentPointer.data) < 0) {
+            currentPointer = currentPointer.NextLN;
+        } else {
+            currentPointer = currentPointer.NextRN;
+        }
+    }
+    if(data.compareTo(currentPointer.data) < 0)
+        currentPointer.NextLN= newNode;
+    else
+        currentPointer.NextRN= newNode;
+}
 
 }
--- BinarySearchMain.java
+++ BinarySearchMain.java
@@ -2,6 +2,10 @@ package com.BridgeLabzDay15;
 
 public class BinarySearchMain {
     public static void main(String[] args) {
-
+        BinarySearch<Integer> bsTree=new BinarySearch<>();
+        //uc1
+        bsTree.add(56);
+        bsTree.add(70);
+        bsTree.add(30);
     }
 }
